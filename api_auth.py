d2d8b07729d38f5c13842992c12bf41fae715e5f
Joe Njogu <joenjogu97@gmail.com>
2021-08-03 19:01:34 +0300

    feat: mentions listener configured

--- api_auth.py
+++ api_auth.py
@@ -1,6 +1,11 @@
 import tweepy
 
-def create_api(consumer_key, consumer_secret, access_token, access_token_secret):
+def create_api(
+    consumer_key = "LbBE7LfWgAH1vWTZFacztjNJx", 
+    consumer_secret = "dxIoZwPvFKx8cWG9azjNVEktCF9VNuLwpKuSOtEJJvqp3Qb0KF", 
+    access_token = "1678385940-JiQ6Uaas6RJ4CRNSnHd8hWTUayx9qIHwTIP2DX2", 
+    access_token_secret = "lfCcxV7Y9enNjLeFGzhFC5J58KDjGIQJP8OobjbnwojH1"
+    ):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
--- mentions_listener.py
+++ mentions_listener.py
@@ -1,1 +1,17 @@
 import tweepy
+import api_auth
+
+def get_mentions(api, count = 20):
+    try:
+        for status in tweepy.Cursor(api.mentions_timeline).pages(count):
+            process_status(status)      
+    except Exception as e:
+        print(e)
+
+def process_status(status):
+    print(count(status))
+
+
+api = api_auth.create_api()
+
+get_mentions(api)
