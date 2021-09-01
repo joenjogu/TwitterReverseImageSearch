c9748ccec2c6d01e84fe25e00052f5644ce40a78
Joe Njogu <joenjogu97@gmail.com>
2021-09-01 18:55:10 +0300

    feat: link for original tweet and its mentiojkl;'n

--- mentions_listener.py
+++ mentions_listener.py
@@ -21,14 +21,16 @@ def get_mentions(api, count = 20):
 def process_status(status):
     status_ids = []
     for tweet in status:
-        status_ids.append(tweet.in_reply_to_status_id)
+        in_reply_to_id = tweet.in_reply_to_status_id
+        status_ids.append(in_reply_to_id)
+        HANDLED_STATUSES.add(in_reply_to_id)
     return status_ids
     
 def get_media_urls(api, latest_status_ids):
     # print(latest_status_ids)
     flattened_status_ids = list(itertools.chain.from_iterable(latest_status_ids))
     status_objects = api.statuses_lookup(flattened_status_ids)
-    print(status_objects)
+    print(len(status_objects), len(flattened_status_ids))
     urls_getter = Url_Retriever.GetTweetMediaUrl(flattened_status_ids)
 
     status_media_urls = {}
@@ -36,9 +38,13 @@ def get_media_urls(api, latest_status_ids):
         urls_list = []
         media_urls = urls_getter.get_media_url(tweet)
         if media_urls:
-            # print(media_urls)
+            # print(media_urls) 
+            tweet_to_mention_link = []
+            for status_id in flattened_status_ids:
+                if tweet.id == status_id: tweet_to_mention_link = [status_id, tweet.id]
+
             urls_list.append(media_urls)
-            new_dict = {tweet.id : urls_list}
+            new_dict = {tweet_to_mention_link : urls_list}
             status_media_urls.update(new_dict)
 
     print(status_media_urls)
